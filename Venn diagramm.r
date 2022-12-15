058f74a6c74403e6680447429c3dddc4e29d8d34
MD UMAR <76789751+MdUmar-tech@users.noreply.github.com>
2022-12-15 22:29:16 +0530

    Create Venn diagramm.r

---
+++ Venn diagramm.r
@@ -0,0 +1,39 @@
+setwd("/Users/mdumar/Desktop/ZZZZZZZZZZZZ_GBK_FUNGUS/ARTEMIS/Velvet_95_Assembly_cazyme_gff")
+library(tidyverse)
+library(stringr)
+library(VennDiagram)
+Cazy<-read.delim("Velvet_95_Assembly_cazy.txt") #import file from directory
+Cazy<-Cazy%>% lapply(str_replace,"\\(.*", "")#remove everything after bracket 
+Cazy<-Cazy%>% lapply(str_replace,"\\_.*", "")
+Cazy<-Cazy%>% lapply(str_replace,"\\+.*", "")
+Cazy<-data.frame(Cazy)
+#Cazy[Cazy == "-"] <- ""#remove - in text file with empty space
+#or
+Cazy[Cazy == "-"] <- "NA" 
+#write.csv(Cazy,"Cazy_ven.csv",row.names=FALSE)#in case to write
+Cazy1<-data.frame(Cazy[,c(1,3,4,5)]) #subset the file
+length(Cazy1) #indicate four row
+
+#count total rows in data frame
+
+nrow(Cazy1[!is.na(Cazy1$HMMER),])#specific column
+#Area1=384
+nrow(Cazy1[!is.na(Cazy1$eCAMI),])
+#Area2=336
+nrow(Cazy1[!is.na(Cazy1$DIAMOND),])
+#Area3=718
+#nrow(na.omit(Cazy1))
+#Area123=281
+nrow(na.omit(Cazy1[,c(2,3)]))
+#Area12=384
+nrow(na.omit(Cazy1[,c(3,4)]))
+#Area23=302
+nrow(na.omit(Cazy1[,c(2,4)]))
+#Area13=333
+draw.triple.venn(area1=384, area2=336, area3=718, 
+                 n12=289, n23=302, n13=333, n123=281, 
+                 category=c("HMMER","eCAMI","DIAMOND"),
+                 col="Red",fill=c("Green","Yellow","Blue"),
+                 cex = 2,cat.cex = 1.5)
+                 
+                 
